89ca3aa10dcf68362495784e2c33a6ea7d82fd96
Groverkss <groverkss@gmail.com>
2021-04-11 06:21:31 +0530

    Added basic cube scene setup

--- src/main.js
+++ src/main.js
@@ -1,27 +1,67 @@
 import * as THREE from 'three'
-import { createSphere } from './geometry'
 
-let scene, camera, renderer
+class Game {
+  constructor() {
+    this.cameraFov = 75; // In degrees
+    this.cameraAspect = 2;
+    this.cameraNear = 0.1;
+    this.cameraFar = 5;
+  }
 
-const init = () => {
-  scene = new THREE.Scene()
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000)
-  renderer = new THREE.WebGLRenderer()
+  init() {
+    /* Camera Setup */
+    this.camera = new THREE.PerspectiveCamera(
+      this.cameraFov, this.cameraAspect, this.cameraNear, this.cameraFar
+    );
+    this.camera.position.z = 3;
 
-  renderer.setSize( window.innerWidth, window.innerHeight )
-  document.body.appendChild( renderer.domElement )
+    /* Scene setup */
+    this.scene = new THREE.Scene();
 
-  var light = new THREE.DirectionalLight( 0xffffff )
-  light.position.set( 1, 1, 1 )
-  scene.add( light )
+    /* Setup render */
+    this.renderer = new THREE.WebGLRenderer();
 
-  const sphere = createSphere({})
-  scene.add(sphere)
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( this.renderer.domElement );
+
+    this.addObjects();
+    this.addLight();
+  }
+
+  addObjects() {
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    const material = new THREE.MeshPhongMaterial({color: 0xFFFF00});
+
+    this.cube = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.cube);
+  }
+
+  addLight() {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+
+    this.scene.add(light);
+  }
+
+  render(time) {
+    time *= 0.001; // Time is given in milliseconds
+
+    this.cube.rotation.x = time;
+    this.cube.rotation.y = time;
+
+    this.renderer.render( this.scene, this.camera );
+
+    requestAnimationFrame(this.render.bind(game));
+  }
 }
 
-const render = () => {
-  renderer.render( scene, camera )
-}
-
-init()
-render()
+const game = new Game();
+game.init();
+requestAnimationFrame(game.render.bind(game));
